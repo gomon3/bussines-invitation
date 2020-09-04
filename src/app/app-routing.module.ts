3744ed0dbcf10f1c2fb763710eaae1c7b90ffdf9
Brandon Gomez <brandonarturo.gomez.contractor@bbva.com>
2020-09-04 16:56:43 -0500

    First version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    component: HomeComponent 
+  },
+  {
+    path: '**', 
+    redirectTo: 'home'
+  },
+  { 
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
