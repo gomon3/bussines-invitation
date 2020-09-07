a9a4385c332df2cbfa5080c5e08c6c3fdbfb5560
Brandon Gomez <brandonarturo.gomez.contractor@bbva.com>
2020-09-07 04:24:27 -0500

    Previus release changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+declare var $: any;
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  days: Number;
+  hours: Number;
+  minutes: Number;
+  seconds: Number;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.initPage();
+    this.initCountdown('12/11/2020');
+  }
+
+  
+  initCountdown(finishDate: string){
+    const dateFinish: Date = new Date(finishDate);
+
+    const source = timer(1000, 1000);
+    const timerScope = source.subscribe(val => {
+
+      let countDownDate = dateFinish.getTime();
+      let now = new Date().getTime();
+
+      let distance = countDownDate - now;
+
+
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    });
+  }
+
+  initPage(){
+    $('.loader').fadeOut();
+    $('#preloder').delay(200).fadeOut("slow");
+
+    $(".mobile-menu").slicknav({
+      prependTo: '#mobile-menu-wrap',
+      allowParentLinks: true
+    });
+  }
+
+  goToLocation(url: any){
+    window.open(url, "_blank");
   }
 
 }
